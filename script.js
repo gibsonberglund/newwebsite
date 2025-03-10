8d5abe78e2e01fc53b6dde8b1a6e9c9669faebb8
Your Name <berglundgibson@gmail.com>
2025-03-10 18:12:55 -0500

    paragraph appears onscreen with clicks

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let background = document.querySelector(".background");
 let sprite = document.querySelector("#sprite");
 let textbox = document.querySelector("#textbox");
 
-let paragraph = "This is the paragraph";
+let paragraph = "I ran to a future city, a metropolis in the year 2320. The polygon skyline shrouded in mist, rainbow lights shining high in the air. A sense of calm and knowing came over me then, as if somehow I’d been there before. Perhaps I’d gone in a circle, and returned to my past, like some kind of cryogenic drifter in a space-time paradox. Someone once told me, in my early-childhood fog, that if you really want to go somewhere you can’t be afraid to get lost. So I slowed to a walk and admired the feeling, admired the familiar strangeness of what I was seeing, strolling under the lamplight glow, the sky overhead a heavy shade of purple. My clothes must have been odd for this place and time, for I received lots of stares from the people passing by. They spoke to each other in short, loud bursts, in a language that sounded like musical notes, chopped up, scrambled, and overlaid with words. I walked down a pavilion on the edge of a park, the city all around lit up in the dark. It occurred to me then that I might not be on Earth. I could have been on some other planet, like a colony on Mars, or one of Jupiter’s moons. Or maybe not even a terrestrial body at all, but the inside of a spaceship the size of a star. I feared, and I hoped, that I was stuck there alone, left to explore this new world and then move on, bouncing from planet to planet and ship to ship, with all of space as my playground and all measures of time irrelevant. An intrepid explorer, the fearless spaceman. Worn but not weary, always curious, courageous, with the wisdom of ages and infinite patience, powered by compassion and boundless fascination. Nothing’s ever too weird, he can never be shocked, the ultimate pioneer on the endless frontier, a force of awakening that cannot be stopped. But then I kept running and soon I found I’d returned to the time I remembered as my own. No longer in the future, or somewhere in space, back on Earth in 2020, daydreaming again.";
 
 let textArray = paragraph.split(" ");
 
@@ -19,22 +19,24 @@ background.addEventListener('click', function() {
     nextWord.innerHTML = textArray[textArrayPosition];
     textbox.appendChild(nextWord);
     textArrayPosition = textArrayPosition + 1; 
+    background.scrollRight += 20;
     } else {
         sprite.style.backgroundColor = "pink";
     }
-})
+});
 
-textbox.addEventListener('click', function() {
-    console.log("hello");
-    if (sprite.style.backgroundColor == "pink") {
-    sprite.style.backgroundColor = "blue";
-    } else {
-        sprite.style.backgroundColor = "pink";
-    }
-})
+// textbox.addEventListener('click', function() {
+//     console.log("hello");
+//     if (sprite.style.backgroundColor == "pink") {
+//     sprite.style.backgroundColor = "blue";
+//     } else {
+//         sprite.style.backgroundColor = "pink";
+//     }
+// })
 
 let xaxis = 0;
 let yaxis = 0;
+
 
 sprite.style.backgroundColor = "pink";
 let flightPath = 30;
